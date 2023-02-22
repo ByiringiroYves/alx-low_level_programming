58b0f74d0b44705adf2f652f4007f1aa59c8f4fd
Byiringiro <byiringiroyves61@gmail.com>
2023-02-21 17:49:19 -0800

    17vqa

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,21 +5,20 @@
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
-int main(void)
-{
-	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+int main() {
+    int num1 = 0, num2 = 1, nextTerm, n;
 
-	while (i < 98)
-	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
-		if (i < 97)
-			printf(", ");
-		i++;
-	}
-	putchar('\n');
-	return (0);
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 1; i <= n; ++i) {
+        printf("%d, ", num1);
+        nextTerm = num1 + num2;
+        num1 = num2;
+        num2 = nextTerm;
+    }
+
+    return 0;
 }
