06e79c84f42f22a7a6ba459074b68d8983f30fd4
Byiringiro <byiringiroyves61@gmail.com>
2023-02-27 10:14:41 -0800

    11qv1

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
 /**
  * main - generates random passwords for 101-crackme
  * Return: zero
  */
+#define PASSWORD_LENGTH 8
 
-int main(void)
-{
-	int sum;
-	char c;
+int main(void) {
+    char password[PASSWORD_LENGTH + 1];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const int charset_size = sizeof(charset) - 1;
 
-	srand(time(NULL));
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
+    srand(time(NULL));
 
-	return (0);
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = charset[rand() % charset_size];
+    }
+    password[PASSWORD_LENGTH] = '\0';
+
+    printf("%s\n", password);
+    return 0;
 }
+
